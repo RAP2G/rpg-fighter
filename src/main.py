af8af8f5e40914c98e12860c6078b2723d80543b
Soma Szabó <szabosonka@gmail.com>
2022-02-23 14:33:29 +0100

    Added char creatoin and items

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
-
-from resources import Character, Goblin
-from random import choice, shuffle
+from resources import Character, Goblin, load_characters, save_character, create_character
+from random import choice, randint, shuffle
 
 
 def fight(players: list, enemies: list):
@@ -30,13 +29,17 @@ def fight(players: list, enemies: list):
 if __name__ == "__main__":
     players = []
     enemies = []
-    vonala = Character("Vonala", 20, 5, 2)
-    nick = Character("Nick", 15, 2, 1)
-    players.append(vonala)
-    players.append(nick)
-    enemies.append(Goblin(10, 3, 1, 1))
-    enemies.append(Goblin(15, 2, 1, 2))
-    enemies.append(Goblin(12, 3, 1, 3))
+    players = load_characters()
+    print("Do you want to create new chracters?")
+    create_new = input("y/n: ").lower()
+    if create_new == "y":
+        how_many = int(input("How many chracters would you like to create?"))
+        for i in range(how_many):
+            players.append(create_character())
+    amount_of_goblins = int(input("How many goblins do you want to fight?"))
+    for i in range(amount_of_goblins):
+        enemies.append(Goblin(randint(10, 15), randint(0, 2), i+1))
+
     fight_round = 1
     while len(players) != 0 or len(enemies) != 0:
         print(enemies)
@@ -49,5 +52,12 @@ if __name__ == "__main__":
 
     if len(players) == 0:
         print("The enemies have won!")
+
     else:
         print("The players have won!")
+        print("Would you like to save the remaining chracters?")
+        save_choice = input("y/n").lower()
+        if save_choice == "y":
+            save_character(players)
+        else:
+            print("No progress has been saved!")
--- src/resources.py
+++ src/resources.py
@@ -1,3 +1,27 @@
+# Imports
+from mimetypes import init
+from random import randint, choice
+
+
+# Global Variables
+
+
+# Classes
+
+class Weapon:
+    def __init__(self, name, damage) -> None:
+        self.name = name
+        self.damage = damage
+
+    def get_damage(self):
+        return self.damage
+
+
+GOBLIN_WEAPONS = [Weapon("Rusty Cleaver", 2),
+                  Weapon("Rusty Spear", 3),
+                  Weapon("Stone Axe", 1)]
+
+
 class Character:
     def __init__(self, name, hp, attack, armor):
         self.name = name
@@ -24,13 +48,17 @@ class Character:
             if self.hp < 0:
                 self.hp = 0
 
+    def get_attributes(self):
+        return self.name, self.hp, self.attack, self.armor
+
 
 class Goblin:
-    def __init__(self, hp, attack, armor, id):
+    def __init__(self, hp, armor, id):
         self.hp = hp
-        self.attack = attack
         self.armor = armor
         self.id = id
+        self.weapon = choice(GOBLIN_WEAPONS)
+        self.attack = self.weapon.get_damage()
 
     def __str__(self):
         return f"Goblin #{self.id}\n HP: {self.hp}\n Attack: {self.attack}\n Armor: {self.armor}"
@@ -51,3 +79,45 @@ class Goblin:
             self.hp -= relative_damage
             if self.hp < 0:
                 self.hp = 0
+
+# functions
+
+
+def save_character(chars: list()):
+    save_list = []
+    for char in chars:
+        name, health, attack, armor = char.get_attributes()
+        save_string = f"{name}/{health}/{attack}/{armor}\n"
+        save_list.append(save_string)
+    with open("saved_characters.txt", "w", encoding="utf8") as f:
+        for char in save_list:
+            f.write(char)
+        print("Characters has been saved!")
+
+
+def load_characters():
+    characters = []
+    with open("saved_characters.txt", "r", encoding="utf8") as f:
+        for line in f.readlines():
+            attributes = line.split("/")
+            char = Character(attributes[0],
+                             int(attributes[1]),
+                             int(attributes[2]),
+                             int(attributes[3]))
+            characters.append(char)
+    return characters
+
+
+def create_character():
+    print("Welcome to the character creator!")
+    name = input("What is your character called?")
+    health = randint(10, 30)
+    attack = randint(1, 5)
+    armor = randint(0, 5)
+
+    return_char = Character(name, health, attack, armor)
+
+    print()
+    print(return_char)
+    print("Character has been created!")
+    return return_char
